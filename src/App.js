59d672d57f8c13ffb2dd2f50bd5a895836b74e78
KarolaBokeh <karolabokeh@protonmail.com>
2022-08-30 10:30:14 -0400

    Added GitHub URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        {" "}
+        This Project was coded by Karen Navarro and is open-sourced on{" "}
+        <a
+          href="https://github.com/KarenPNavarro/react-weather-project"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
