5ec74dc9c68f7ce4e0ad2ab69a03bd981d1cfe19
KarolaBokeh <karolabokeh@protonmail.com>
2022-08-30 10:57:56 -0400

    Added NPM Bootstrap pckg, Created new Weather file

--- src/App.js
+++ src/App.js
@@ -5,17 +5,24 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        {" "}
-        This Project was coded by Karen Navarro and is open-sourced on{" "}
-        <a
-          href="https://github.com/KarenPNavarro/react-weather-project"
-          target="_blank"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          {" "}
+          This Project was coded by{" "}
+          <a href="https://karennavarro.io/" target="_blank">
+            Karen Navarro
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/KarenPNavarro/react-weather-project"
+            target="_blank"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
