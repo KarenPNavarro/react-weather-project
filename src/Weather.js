75d9c7ba8f6ad2f81fc7017ac2b22a4fff704dbb
KarolaBokeh <karolabokeh@protonmail.com>
2022-08-30 11:17:44 -0400

    Added forms and submit button

--- src/Weather.js
+++ src/Weather.js
@@ -1,3 +1,44 @@
 import React from "react";
+import "./Weather.css";
 
-export default function Weather() {}
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Tuesday 11:00</li>
+        <li>Sunny</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+            alt="Sunny"
+          />
+          28ºC
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 65%</li>
+            <li>Wind: 11 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
